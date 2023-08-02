681f1b5c2f5a0b95ff548eb293943e2c77a3ea3d
matan.rahav <matan.rahav@syte-vc.com>
2023-08-02 16:33:59 +0300

    wip word clould service

--- word-cloud-api/src/services/word-cloud.service.ts
+++ word-cloud-api/src/services/word-cloud.service.ts
@@ -1,13 +1,5 @@
 import axios from 'axios'
 
-const getClassName = async ():Promise<string>=>{
-    return await axios.get('')
-}
-
-export const splitPascalCase = (string:string):string[]=>{
-    return string.replace(/[A-Z]/g,' $&').trim().split(' ')
-}
- 
 export type WordCloud = Record<string,number>
 
 // export type WordCloudEntry = {
@@ -16,6 +8,33 @@ export type WordCloud = Record<string,number>
 // }
 // export type WordCloud = WordCloudEntry[]
 
-export const generateWordCloud = async ():Promise<WordCloud>=>{
 
+const getRandomPascalCaseString = async ():Promise<string>=>{
+    //return await axios.get('')
+    return Promise.resolve('SomePascalCase')
+}
+
+const getRandomPascalCaseStrings = async (size:number):Promise<string[]>=>{
+    const promises = new Array(size).fill(null).map(getRandomPascalCaseString)
+    return await Promise.all(promises)
+}
+
+export const splitPascalCase = (string:string):string[]=>{
+    return string.replace(/[A-Z]/g,' $&').trim().split(' ').map(str=>str.toLocaleLowerCase())
+}
+ 
+export const generateWordCloud = async ():Promise<WordCloud>=>{
+    const pascalCaseStrings = await getRandomPascalCaseStrings(2)
+    const wordMap = pascalCaseStrings.reduce((acc,pascalString)=>{
+        const words = splitPascalCase(pascalString)
+        words.forEach(word=>{
+            if(!acc[word]){
+                acc[word] = 0
+            }
+            acc[word]++
+        })
+        return acc
+    },{})
+
+    return wordMap
 }
